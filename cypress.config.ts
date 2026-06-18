b30684ab9641d1f2a5d9ea7edc2f15e5a0570eb1
Daniel V. <niel.vicente98@gmail.com>
2026-06-17 22:02:25 -0500

    test(e2e): clase 2 -setup de cypress y primeros tes"

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    baseUrl: "https://practice.expandtesting.com",
+    viewportWidth: 1280,
+    setupNodeEvents(on, config) {
+      // implement node event listener here
+    },
+  },
+});
