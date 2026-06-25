652d88944c4777eefae73b73b4f3b89cae6069b3
Carlos R. <>
2026-06-24 22:31:27 -0500

    test(e2e): clase4 - estabilidad pom commands e intercept

---
+++ cypress/pages/clase4/LoginPage.ts
@@ -0,0 +1,23 @@
+export class LoginPage {
+  private readonly url = "/login";
+  private readonly selectors = {
+    usernameInput: "username",
+    passwordInput: "password",
+    loginButton: "submit-login",
+    errorMessage: ".alert-danger",
+  } as const;
+
+  visit(): void {
+    cy.visit(this.url);
+  }
+
+  errorMessage() {
+    return cy.get(this.selectors.errorMessage);
+  }
+
+  login(username: string, password: string): void {
+    cy.getById(this.selectors.usernameInput).type(username);
+    cy.getById(this.selectors.passwordInput).clear().type(password);
+    cy.getById(this.selectors.loginButton).click();
+  }
+}
