3781fb5daacc7ebdd2050514e59a61fff39980d5
Daniel V. <niel.vicente98@gmail.com>
2026-06-24 22:31:39 -0500

    test(e2e): clase 4 - estabilidad pom commands e intercept

---
+++ cypress/e2e/clase4/dropdown-colecciones.cy.ts
@@ -0,0 +1,26 @@
+describe("Dropdown - selectores avanzados y colecciones", () => {
+  
+    beforeEach(() => {
+    cy.visit("/dropdown");
+  })
+
+  it("debería seleccionar una opción por texto visible", () => {
+
+    cy.get("#dropdown").should("be.visible")
+    cy.get("#dropdown").select("Option 1")
+    cy.get("#dropdown").should("have.value", "1")
+  })
+
+  it("debería validar opciones usando colecciones", () => {
+
+    cy.get("#dropdown option").should("have.length.at.least", 3)
+    
+    cy.get("#dropdown option").eq(1).should("have.value", "1").and("contain.text", "Option 1")
+
+    cy.get("#dropdown option").each(($option, index) => {
+      cy.log(`Validando opción ${index}`)
+      cy.wrap($option).should("exist")
+      cy.wrap($option).should("not.be.empty")
+    })
+  })
+})
