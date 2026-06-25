4ee177e93cd0bfb376d238af2e9c18e837de5e1d
Jenny Lopez <jenny.lopez.melendez26@gmail.com>
2026-06-24 22:28:18 -0500

    test(e2e):clase4 - estabilidad pom commands e intercept

---
+++ cypress/e2e/clase4/login-pom-intercept.cy.ts
@@ -0,0 +1,25 @@
+import { LoginPage } from "../../pages/clase4/LoginPage";
+
+const loginPage = new LoginPage();
+
+describe("Login- POM, commands e intercept", () => {
+
+  beforeEach(() => {
+    loginPage.visit();
+  });
+
+  it("debería iniciar sesión con credenciales válidas", () => {
+    cy.intercept("POST", "**/authenticate").as("loginRequest");
+    
+    loginPage.login("practice", "SuperSecretPassword!");
+
+    cy.wait("@loginRequest").its("response.statusCode").should("be.oneOf", [200, 302, 303]);
+    cy.url().should("include", "/secure");
+    cy.contains("h1", "Secure Area").should("be.visible");
+  });
+
+  it("debería mostrar error con credenciales inválidas", () => {
+    loginPage.login("usuario_invalido", "password_invalido");
+    loginPage.errorMessage().should("be.visible").and("contain.text", "invalid");
+  });
+});
---
+++ cypress/pages/clase4/LoginPage.ts
@@ -0,0 +1,25 @@
+export class LoginPage {
+
+  private readonly url = "/login";
+
+  private readonly selectors = {
+    usernameInput: "username",
+    passwordInput: "password",
+    loginButton: "submit-login",
+    errorMessage: ".alert-danger",
+  } as const;
+
+  visit(): void {
+    cy.visit(this.url);
+  }
+
+  errorMessage() {
+    return cy.get(this.selectors.errorMessage);
+  }
+  
+  login(username: string, password: string): void {
+    cy.getById(this.selectors.usernameInput).clear().type(username);
+    cy.getById(this.selectors.passwordInput).clear().type(password);
+    cy.getById(this.selectors.loginButton).click();
+  }
+}
