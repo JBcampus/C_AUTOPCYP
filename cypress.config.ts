2fe256ac5a01e96df48e479fca7b49cb0f677d0f
AdelinaB <aabravoc@gmail.com>
2026-09-21 22:01:41 -0500

    test(clase3): uso de hooks y fixture

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  screenshotsFolder: "artifacts/screenshots",
+  videosFolder: "artifacts/videos",
+  downloadsFolder: "artifacts/downloads",
+  screenshotOnRunFailure: true,
+  video: false,
+  trashAssetsBeforeRuns: true,
+
   e2e: {
     baseUrl: "https://practice.expandtesting.com",
     viewportWidth: 1280,
